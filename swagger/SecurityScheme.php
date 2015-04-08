61169475f5672de67cf032a0366721b723ed1526
m8rge <to.merge@gmail.com>
2015-04-08 17:48:17 +0500

    authorization

---
+++ swagger/SecurityScheme.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace e96\swagger;
+
+
+class SecurityScheme extends Object
+{
+    /**
+     * @var string
+     */
+    public $type;
+    /**
+     * @var string
+     */
+    public $description;
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $in;
+}
